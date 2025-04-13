f32ac38d9f78389bba5666bbd29d7b2098c42cc3
DevMadrine <madrinenamulindwa90@gmail.com>
2025-04-13 11:21:50 +0300

    using super keyword in java

---
+++ src/oop/superkeyword/Hero.java
@@ -0,0 +1,13 @@
+package oop.superkeyword;
+
+public class Hero extends Person {
+    String power;
+    Hero(String name, int age,String power){
+        super(name, age);
+        this.power = power;
+    }
+
+    public String toString(){
+        return this.name + "\n" +this.age + "\n" +this.power +"\n";
+    }
+}
---
+++ src/oop/superkeyword/Main.java
@@ -0,0 +1,8 @@
+package oop.superkeyword;
+
+public class Main {
+    public static void main(String[]args){
+        Hero hero = new Hero("Madrine", 14, "jjj");
+        System.out.println(hero.toString());
+    }
+}
